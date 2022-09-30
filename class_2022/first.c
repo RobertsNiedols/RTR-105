c04ce07d43632b8048c415987ba1492862bd5e7f
RobertsNiedols <r.niedols@gmail.com>
2022-09-30 11:52:27 +0300

    Class_06

---
+++ class_2022/first.c
@@ -0,0 +1,21 @@
+
+// komentāra viena rinda - // tiek apzīmets tikai komentāra sākums
+// komentāra cita rinda
+
+/*
+Komentāra paragrāfs
+izmanto, piemēram, lai komentētu koda fragmentus
+*/
+
+int main()
+{
+//if()
+ //{
+ //
+ //}
+
+return 0;
+
+
+//return 100;
+}
